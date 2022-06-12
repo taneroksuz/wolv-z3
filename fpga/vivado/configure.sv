77350676d1ce59e840d8a3e22182b9aec6c09665
Taner Öksüz <taner.oeksuez@outlook.de>
2022-06-12 11:56:45 +0200

    some errors fixed. design optimized.

--- fpga/vivado/configure.sv
+++ fpga/vivado/configure.sv
@@ -15,10 +15,6 @@ package configure;
   parameter dtim_width = 2;
   parameter dtim_depth = 10;
 
-  parameter clint_contexts = 0;
-
-  parameter plic_contexts = 0;
-
   parameter bram_base_addr = 32'h0;
   parameter bram_top_addr  = 32'h8000;
 
@@ -33,9 +29,6 @@ package configure;
 
   parameter clint_base_addr = 32'h2000000;
   parameter clint_top_addr  = 32'h200C000;
-
-  parameter plic_base_addr = 32'h0C000000;
-  parameter plic_top_addr  = 32'h10000000;
 
   parameter clk_freq = 100000000; // 100MHz
   parameter clk_pll = 50000000; // 50MHz
