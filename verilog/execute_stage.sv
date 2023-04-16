28a9de2312eabedcfd7983e9be401f73ce32d52e
Taner Öksüz <taner.oeksuez@outlook.de>
2023-04-16 10:14:51 +0200

    pipeline optimized.

--- verilog/execute_stage.sv
+++ verilog/execute_stage.sv
@@ -89,7 +89,7 @@ module execute_stage
       v = r;
     end
 
-    v.clear = d.e.clear;
+    v.clear = csr_out.trap | csr_out.mret | d.e.clear;
 
     v.stall = 0;
 
@@ -180,7 +180,7 @@ module execute_stage
       end
     end
 
-    if ((v.stall | v.clear | csr_out.trap | csr_out.mret) == 1) begin
+    if ((v.stall | v.clear) == 1) begin
       v.wren = 0;
       v.cwren = 0;
       v.auipc = 0;
